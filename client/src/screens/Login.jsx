2137370729aac145032717094f2bcb2ecb51a9b1
Daynamarie <Dayna.christison@gmail.com>
2020-10-13 16:03:58 -0400

    login and register working - created main to render hello

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,40 +1,56 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const Login = () => {
-    const [formData, setFormData] = useState({
-        username: '',
-        password: ''
-    })
-    const {username, password} = formData
-    
-    return(
-        <>
-            <form>
-                <h2>Login</h2>
-                <label>Username:
-                <input
-                    type='text'
-                    name='username'
-                    value={username}
-                />       
-                </label>
-                <label>Password:
-                <input
-                    type='password'
-                    name='password'
-                    value={password}
-                />
-                </label>
-                <button>Enter if you DARE</button>                
-            </form>
-            <h5>New to Symptomatic?</h5>
-            <Link to='/register'>
-                <h5>Register Here</h5>
-            </Link>
+const Login = (props) => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = formData;
+  const { handleLogin } = props;
 
-        </>
-    )
-}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  return (
+    <>
+      <form
+        onSubmit={(e) => { //track an event
+          e.preventDefault(); //but prevent it from happening on render
+          handleLogin(formData); //set state of handle function to the
+        }}
+      >
+        <h2>Login</h2>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </label>
+        <button>Enter if you DARE</button>
+      </form>
+      <h5>New to Symptomatic?</h5>
+      <Link to="/register">
+        <h5>Register Here</h5>
+      </Link>
+    </>
+  );
+};
 
-export default Login
+export default Login;
--- client/src/screens/Register.jsx
+++ client/src/screens/Register.jsx
@@ -1,40 +1,55 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const Register = () => {
-    const [formData, setFormData] = useState({
-        username: '',
-        password: ''
-    })
-    const {username, password} = formData
-    
-    return(
-        <>
-            <form>
-                <h2>Register</h2>
-                <label>Username:
-                <input
-                    type='text'
-                    name='username'
-                    value={username}
-                />       
-                </label>
-                <label>Password:
-                <input
-                    type='password'
-                    name='password'
-                    value={password}
-                />
-                </label>
-                <button>Become Fam</button>                
-            </form>
-            <h5>Already Symptomatic?</h5>
-            <Link to='/'>
-                <h5>Login Here</h5>
-            </Link>
+const Register = (props) => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const {username, password} = formData;
+  const {handleRegister} = props
+  const handleChange = (e) => { //when are we changing it
+    const { name, value } = e.target; //what are we changing when we're envoked
+    setFormData((prevState) => ({ //what state are we changing it to
+      ...prevState, //how do we insert it
+      [name]: value, //how it it being saved
+    }));
+  };
 
-        </>
-    )
-}
+  return (
+    <>
+      <form
+        onSubmit={ (e) => {
+            e.preventDefault()
+            handleRegister(formData)
+        }}>
+        <h2>Register</h2>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </label>
+        <button>Become Fam</button>
+      </form>
+      <h5>Already Symptomatic?</h5>
+      <Link to="/">
+        <h5>Login Here</h5>
+      </Link>
+    </>
+  );
+};
 
-export default Register
+export default Register;
