a2b504b3e2c3f09a6789d472ec2dfbe15573c108
Daynamarie <Dayna.christison@gmail.com>
2020-10-15 15:20:33 -0400

    cleaning up main container / routes

--- client/src/containers/MainContainer.jsx
+++ client/src/containers/MainContainer.jsx
@@ -1,47 +1,38 @@
-import React, {useEffect, useState} from 'react'
-import { Route, Switch, useHistory } from "react-router-dom";
-import Main from '../screens/Main';
-import MySymptoms from '../screens/MySymptoms';
-import NewSymptom from '../screens/NewSymptom';
-import { getAllSymptoms } from '../services/symptoms';
-
+import React, { useEffect, useState } from "react";
+import { Route, Switch, useHistory, Link } from "react-router-dom";
+import Main from "../screens/Main";
+import MySymptoms from "../screens/MySymptoms";
+import NewSymptom from "../screens/NewSymptom";
+import { getAllSymptoms } from "../services/symptoms";
 
 const MainContainer = (props) => {
-    const {currentUser, handleLogout} = props
+  const { currentUser, handleLogout } = props;
 
-    const [symptoms, setSymptoms] = useState([])
+  const [symptoms, setSymptoms] = useState([]);
 
-    useEffect(()=>{
-        const fetchSymptom = async () => {
-            const symptomsArr = await getAllSymptoms();
-            setSymptoms(symptomsArr)
-        }
-        if (currentUser){
-            fetchSymptom()
-        }
-    }, [currentUser])
+  useEffect(() => {
+    const fetchSymptom = async () => {
+      const symptomsArr = await getAllSymptoms();
+      setSymptoms(symptomsArr);
+    };
+    fetchSymptom();
+  }, []);
 
-/*     const createSubmit = async(formData) => {
+  /* const createSubmit = async(formData) => {
         const addSymptom = await postSymptom(formData)
         let symptom_id = addSymptom.id
 
         setSymptoms((prevState) => [...prevState, addSymptom])
         history.push("/symptoms")
-    }
- */
+    } */
 
-    return(
-        <Switch>
-            {/* <Route path="/symptoms/add">
-                <NewSymptom 
-                    createSubmit={createSubmit}
-                />
-            </Route> */}
-            <Route path="/main">
-                <Main symptoms={symptoms}/>
-            </Route>
-        </Switch>
-    )
-}
+  return (
+    <Switch>
+      <Main currentUser={currentUser} symptoms={symptoms} />
 
-export default MainContainer
+      <MySymptoms symptoms={symptoms} currentUser={currentUser} />
+    </Switch>
+  );
+};
+
+export default MainContainer;
--- client/src/screens/Main.jsx
+++ client/src/screens/Main.jsx
@@ -1,39 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Layout from "../shared/Layout";
+import MySymptoms from "./MySymptoms";
 
 const Main = (props) => {
-  const { currentUser, handleLogout } = props;
+  const {symptoms, currentUser} = props;
 
   return (
     <>
-      <h2>Hello!</h2>
-
-
-      <Link>
-        <div>
-          <h5>My Calendar</h5>
-        </div>
-      </Link>
-
+      <h2>PAGE : MAIN</h2>
 
       <div className="all-symptoms">
-        
-      
-            <Link to="/symptoms">
-              <div>
-                <h5>My Symptoms</h5>
-              </div>
-            </Link>
-        
+        <Link to="/symptoms">
+          <div>
+            <h5>My Symptoms</h5>
+          </div>
+        </Link>
       </div>
 
-
-      <Link>
+      
         <div>
           <h5>New Symptom</h5>
         </div>
-      </Link>
-      
+    
     </>
   );
 };
