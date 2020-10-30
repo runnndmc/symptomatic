842bb2e96685e7a67c20f2bcdd994ed2a65b6975
Daynamarie <Dayna.christison@gmail.com>
2020-10-30 16:39:04 -0400

    adding symptoms and bringint back to all symptoms page

--- client/src/screens/AddSymptom.jsx
+++ client/src/screens/AddSymptom.jsx
@@ -1,13 +1,68 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
+const AddSymptom = (props) => {
+  const { createSubmit } = props;
+  const [formData, setFormData] = useState({
+    symptom: "",
+    description: "",
+    pain_level: "",
+  });
+  const { symptom, description, pain_level } = formData;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-const AddSymptom = () => {
+  return (
+    <div className="create-form">
+      <h2> add new symptom HERE</h2>
 
-    return(
-        <>
-            <h2> add new symptom HERE</h2>
-        </>
-    )
-}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          createSubmit(formData);
+          setFormData({
+            symptom: "",
+            description: "",
+            pain_level: "",
+          });
+        }}
+      >
+        <label>
+          Type of Symptom:
+          <input
+            type="text"
+            name="symptom"
+            value={symptom}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Describe more about it:
+          <input
+            type="textarea"
+            name="description"
+            value={description}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Pain level:
+          <input
+            type="text"
+            name="pain_level"
+            value={pain_level}
+            onChange={handleChange}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
 
-export default AddSymptom
+export default AddSymptom;
